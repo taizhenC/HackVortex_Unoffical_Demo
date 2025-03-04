0f66dd2800f11ae1b7ec2c68e1da495c5e9a4efc
Zhicheng Yu <yuzhicheng369@gmail.com>
2025-03-03 21:47:17 -0500

    basic setup complete

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { motion } from 'framer-motion';
 function App() {
   return (
     <div className="App">
---
+++ my-app/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './src/**/*.{html,js,jsx,ts,tsx}',  // Path to your components
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
