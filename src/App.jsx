19f4748e43bd822119f7c9ba387a1273edead9f7
taizhen <tzhch>
2025-03-29 15:55:15 -0400

    finish the tictactoe

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./web_page/Home.jsx";
 import Task from "./web_page/Task.jsx";
+import TicTacToe from "./web_page/TicTacToe.jsx";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/task" element={<Task />} />
+        <Route path="/tictactoe" element={<TicTacToe />} />
       </Routes>
     </>
   );
