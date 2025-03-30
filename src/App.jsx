21d5db7e1d958b50a536df309405a4597457a951
taizhen <tzhch>
2025-03-30 17:20:29 -0400

    tzc:finish another game demo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./web_page/Home.jsx";
 import Task from "./web_page/Task.jsx";
 import TicTacToe from "./web_page/TicTacToe.jsx";
+import WordCompletionGame from "./web_page/WordCompletionGame.jsx";
 //yzc
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/task" element={<Task />} />
         <Route path="/tictactoe" element={<TicTacToe />} />
+        <Route path="/word-game" element={<WordCompletionGame />} />
       </Routes>
     </>
   );
